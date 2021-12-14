4fb5c460e7ec1ac02e3eb314b5ad5590f8da97cd
Sean Walsh <seanmichwalsh@protonmail.ch>
2021-12-14 01:43:03 -0500

    Final implementation of day 1

---
+++ src/day1.rs
@@ -0,0 +1,47 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use std::error::Error;
+
+fn part_one(depths: &Vec<u32>) -> u32 {
+    let mut inc_count: u32 = 0;
+
+    for pair in depths.iter().zip(depths.iter().skip(1)) {
+        let prev = pair.0;
+        let next = pair.1;
+        if next > prev {
+            inc_count += 1;
+        }
+    }
+    
+    return inc_count;
+}
+
+fn part_two(depths: &Vec<u32>) -> u32 {
+    let mut inc_count: u32 = 0;
+
+    for window in depths.windows(3).zip(depths.windows(3).skip(1)) {
+        let prev_sum: u32 = window.0.iter().sum();
+        let next_sum: u32 = window.1.iter().sum();
+        if next_sum > prev_sum {
+            inc_count += 1;
+        }
+    }
+
+    return inc_count;
+}
+
+pub fn solve() -> Result<(String, String), Box<dyn Error>> {
+    let input = File::open("./input/day1.txt")?;
+    let reader = BufReader::new(input);
+    let mut depths: Vec<u32> = Vec::new();
+
+    for line in reader.lines() {
+        let value = line.unwrap();
+        depths.push(value.parse::<u32>()?);
+    }
+
+    let part_one = part_one(&depths).to_string();
+    let part_two = part_two(&depths).to_string();
+
+    Ok((part_one, part_two))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+mod day1;
+use std::env;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        return Err("Please enter the day to solve for!".into());
+    }
+
+    let day = &args[1].parse::<u32>()?;
+
+    let result = match day {
+        1 => day1::solve(),
+        2..=25 => return Err("Day not yet implemented!".into()),
+        _ => return Err("Advent of Code only runs from Dec 1-25!".into()),
+    };
+
+    let result = result.unwrap();
+    println!("Day {} - Part 1: {}", day, result.0);
+    println!("Day {} - Part 2: {}", day, result.1);
+
+    Ok(())
 }
